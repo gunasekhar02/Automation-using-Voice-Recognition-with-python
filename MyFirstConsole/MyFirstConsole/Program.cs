5d8b3e7a6119ad829cfd3361a932b7df31b6a3a3
Guna Reddy <Guna.Reddy@gep.com>
2023-07-07 19:31:07 +0530

    hello

---
+++ MyFirstConsole/MyFirstConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyFirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 90;
+            int temp;
+            temp = a;
+            a = b;
+            b= temp;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
